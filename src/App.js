964584086f4ba200aff72d0012b5b57b0cc51f1d
m6ydz642 <m6ydz642@gmail.com>
2020-11-07 03:08:31 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+  <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" />
+  <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+
+
+    </div>
+  );
+}
+
+export default App;
